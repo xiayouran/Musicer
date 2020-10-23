48c707c978faefb3822d40e230e5f588d104aa95
xiayouran <youran.xia@foxmail.com>
2020-10-24 01:29:35 +0800

    first submit WangYiYunMusic and KuGouMusic V1.0 2020/10/24

---
+++ KuGouMusic/KuGouMusic.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/24 1:20
+# @Author  : XiaYouRan
+# @Email   : youran.xia@foxmail.com
+# @File    : KuGouMusic.py
+# @Version : V1.0
+# @Software: PyCharm
+
+
+import time
+from hashlib import md5
+import json
+import requests
+import re
+import os
+
+
+class KuGouMusic(object):
+    def __init__(self):
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+
+    def MD5Encrypt(self, text):
+        # 返回当前时间的时间戳(1970纪元后经过的浮点秒数)
+        k = time.time()
+        k = int(round(k * 1000))
+        info = ["NVPh5oo715z5DIWAeQlhMDsWXXQV4hwt", "bitrate=0", "callback=callback123",
+                "clienttime={}".format(k), "clientver=2000", "dfid=-", "inputtype=0",
+                "iscorrection=1", "isfuzzy=0", "keyword={}".format(text), "mid={}".format(k),
+                "page=1", "pagesize=30", "platform=WebFilter", "privilege_filter=0",
+                "srcappid=2919", "tag=em", "userid=-1", "uuid={}".format(k), "NVPh5oo715z5DIWAeQlhMDsWXXQV4hwt"]
+        # 创建md5对象
+        new_md5 = md5()
+        info = ''.join(info)
+        # 更新哈希对象
+        new_md5.update(info.encode(encoding='utf-8'))
+        # 加密
+        signature = new_md5.hexdigest()
+        url = 'https://complexsearch.kugou.com/v2/search/song?callback=callback123&keyword={0}' \
+              '&page=1&pagesize=30&bitrate=0&isfuzzy=0&tag=em&inputtype=0&platform=WebFilter&userid=-1' \
+              '&clientver=2000&iscorrection=1&privilege_filter=0&srcappid=2919&clienttime={1}&' \
+              'mid={2}&uuid={3}&dfid=-&signature={4}'.format(text, k, k, k, signature.upper())
+        return url
+
+    def get_html(self, url):
+        # 加一个cookie
+        cookie = 'kg_mid=61a73ea098eb98e7c6f4fbc66cd7f367; kg_dfid=3LfODQ2G5XMN0x1liv3DeyjX; kg_dfid_collect=d41d8cd98f00b204e9800998ecf8427e; Hm_lvt_aedee6983d4cfc62f509129360d6bb3d=1599906321; Hm_lpvt_aedee6983d4cfc62f509129360d6bb3d=1599922649'.split(
+            '; ')
+        cookie_dict = {}
+        for co in cookie:
+            co_list = co.split('=')
+            cookie_dict[co_list[0]] = co_list[1]
+        try:
+            response = requests.get(url, headers=self.headers, cookies=cookie_dict)
+            response.raise_for_status()
+            response.encoding = 'utf-8'
+            return response.text
+        except Exception as err:
+            print(err)
+            return '请求异常'
+
+    def parse_text(self, text):
+        count = 0
+        hash_list = []
+        print('{:*^80}'.format('搜索结果如下'))
+        print('{0:{5}<5}{1:{5}<15}{2:{5}<10}{3:{5}<10}{4:{5}<20}'.format('序号', '歌名', '歌手', '时长(s)', '专辑', chr(12288)))
+        print('{:-^84}'.format('-'))
+        song_list = json.loads(text)['data']['lists']
+        for song in song_list:
+            singer_name = song['SingerName']
+            # <em>本兮</em> 正则提取
+            # 先匹配'</em>'这4中字符, 然后将其替换
+            pattern = re.compile('[</em>]')
+            singer_name = re.sub(pattern, '', singer_name)
+            song_name = song['SongName']
+            song_name = re.sub(pattern, '', song_name)
+            album_name = song['AlbumName']
+            # 时长
+            duration = song['Duration']
+            file_hash = song['FileHash']
+            file_size = song['FileSize']
+
+            # 音质为HQ, 高品质
+            hq_file_hash = song['HQFileHash']
+            hq_file_size = song['HQFileSize']
+
+            # 音质为SQ, 超品质, 即无损, 后缀为flac
+            sq_file_hash = song['SQFileHash']
+            sq_file_size = song['SQFileSize']
+
+            # MV m4a
+            mv_hash = song['MvHash']
+            m4a_size = song['M4aSize']
+
+            hash_list.append([file_hash, hq_file_hash, sq_file_hash])
+
+            print('{0:{5}<5}{1:{5}<15}{2:{5}<10}{3:{5}<10}{4:{5}<20}'.format(count, song_name, singer_name, duration, album_name,
+                                                                             chr(12288)))
+            count += 1
+            if count == 10:
+                # 为了测试方便, 这里只显示了10条数据
+                break
+        print('{:*^80}'.format('*'))
+        return hash_list
+
+    def save_file(self, song_text):
+        filepath = './download'
+        if not os.path.exists(filepath):
+            os.mkdir(filepath)
+        text = json.loads(song_text)['data']
+        audio_name = text['audio_name']
+        author_name = text['author_name']
+        album_name = text['album_name']
+        img_url = text['img']
+        lyrics = text['lyrics']
+        play_url = text['play_url']
+        response = requests.get(play_url, headers=self.headers)
+        with open(os.path.join(filepath, audio_name) + '.mp3', 'wb') as f:
+            f.write(response.content)
+            print("下载完毕!")
+
+
+if __name__ == '__main__':
+    kg = KuGouMusic()
+    search_info = input("请输入歌名或歌手: ")
+    search_url = kg.MD5Encrypt(search_info)
+
+    search_text = kg.get_html(search_url)
+    hash_list = kg.parse_text(search_text[12:-2])
+
+    while True:
+        input_index = eval(input("请输入要下载歌曲的序号(-1退出): "))
+        if input_index == -1:
+            break
+        download_info = hash_list[input_index]
+        song_url = 'https://wwwapi.kugou.com/yy/index.php?r=play/getdata&hash={}'.format(download_info[0])
+        song_text = kg.get_html(song_url)
+        kg.save_file(song_text)
+
---
+++ WangYiYunMusic/WangYiYunMusic.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/24 0:30
+# @Author  : XiaYouRan
+# @Email   : youran.xia@foxmail.com
+# @File    : WangYiYunMusic.py
+# @Version : V1.0
+# @Software: PyCharm
+
+
+import requests
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+import random
+import base64
+import json
+import os
+
+
+class EncryptText:
+    def __init__(self):
+        self.character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        self.iv = '0102030405060708'
+        self.public_key = '010001'
+        self.modulus = '00e0b509f6259df8642dbc35662901477df22677ec152b' \
+                       '5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417' \
+                       '629ec4ee341f56135fccf695280104e0312ecbda92557c93' \
+                       '870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b' \
+                       '424d813cfe4875d3e82047b97ddef52741d546b8e289dc69' \
+                       '35b3ece0462db0a22b8e7'
+        self.nonce = '0CoJUm6Qyw8W8jud'
+
+    def create16RandomBytes(self):
+        """
+        # 产生16位随机字符, 对应函数a
+        :return:
+        """
+        generate_string = random.sample(self.character, 16)
+        generated_string = ''.join(generate_string)
+        return generated_string
+
+    def AESEncrypt(self, clear_text, key):
+        """
+        AES加密, 对应函数b
+        :param clear_text: 需要加密的数据
+        :return:
+        """
+        # 数据填充
+        clear_text = pad(data_to_pad=clear_text.encode(), block_size=AES.block_size)
+        key = key.encode()
+        iv = self.iv.encode()
+        aes = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
+        cipher_text = aes.encrypt(plaintext=clear_text)
+        # 字节串转为字符串
+        cipher_texts = base64.b64encode(cipher_text).decode()
+        return cipher_texts
+
+    def RSAEncrypt(self, i, e, n):
+        """
+        RSA加密, 对应函数c
+        :param i:
+        :return:
+        """
+        # num = pow(x, y) % z
+        # 加密C=M^e mod n
+        num = pow(int(i[::-1].encode().hex(), 16), int(e, 16), int(n, 16))
+        result = format(num, 'x')
+        return result
+
+    def resultEncrypt(self, input_text):
+        """
+        对应函数d
+        :param input_text:
+        :return:
+        """
+        i = self.create16RandomBytes()
+        # i = "g7E16vXN1O2T3o7V"
+        encText = self.AESEncrypt(input_text, self.nonce)
+        encText = self.AESEncrypt(encText, i)
+        # print(len(encText))
+        # print(encText)
+        # encSecKey = self.RSAEncrypt(i.encode())
+        encSecKey = self.RSAEncrypt(i, self.public_key, self.modulus)
+        # print(len(encSecKey))
+        # print(encSecKey)
+        from_data = {
+            'params': encText,
+            'encSecKey': encSecKey
+        }
+        return from_data
+
+
+class WangYiYunMusic(object):
+    def __init__(self):
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+
+    def get_html(self, url, method='GET', from_data=None):
+        try:
+            if method == 'GET':
+                response = requests.get(url, headers=self.headers)
+            else:
+                response = requests.post(url, from_data, headers=self.headers)
+            response.raise_for_status()
+            response.encoding = 'utf-8'
+            return response.text
+        except Exception as err:
+            print(err)
+            return '请求异常'
+
+    def parse_text(self, text):
+        ids_list = json.loads(text)['result']['songs']
+        count = 0
+        info_list = []
+        print('{:*^80}'.format('搜索结果如下'))
+        print('{0:{5}<5}{1:{5}<20}{2:{5}<10}{3:{5}<10}{4:{5}<20}'.format('序号', '歌名', '歌手', '时长(s)', '专辑', chr(12288)))
+        print('{:-^84}'.format('-'))
+        for id_info in ids_list:
+            song_name = id_info['name']
+            id = id_info['id']
+            time = id_info['dt'] // 1000
+            album_name = id_info['al']['name']
+            picture_url = id_info['al']['picUrl']
+            singer = id_info['ar'][0]['name']
+            info_list.append([id, song_name, singer])
+            print('{0:{5}<5}{1:{5}<20}{2:{5}<10}{3:{5}<10}{4:{5}<20}'.format(count, song_name, singer, time, album_name, chr(12288)))
+            count += 1
+            if count == 8:
+                # 为了测试方便, 这里只显示了9条数据
+                break
+        print('{:*^80}'.format('*'))
+        return info_list
+
+    def save_file(self, song_text, download_info):
+        filepath = './download'
+        if not os.path.exists(filepath):
+            os.mkdir(filepath)
+        filename = download_info[1] + '-' + download_info[2]
+        music_url = json.loads(song_text)['data'][0]['url']
+        response = requests.get(music_url, headers=self.headers)
+        with open(os.path.join(filepath, filename) + '.mp3', 'wb') as f:
+            f.write(response.content)
+            print("下载完毕!")
+
+
+if __name__ == '__main__':
+    id_url = 'https://music.163.com/weapi/cloudsearch/get/web?csrf_token='
+    song_url = 'https://music.163.com/weapi/song/enhance/player/url/v1?csrf_token='
+
+    id_d = {
+        "hlpretag": "<span class=\"s-fc7\">",
+        "hlposttag": "</span>",
+        "s": input("请输入歌名或歌手: "),
+        "type": "1",
+        "offset": "0",
+        "total": "true",
+        "limit": "30",
+        "csrf_token": ""
+    }
+
+    encrypt = EncryptText()
+    id_from_data = encrypt.resultEncrypt(str(id_d))
+
+    wyy = WangYiYunMusic()
+    id_text = wyy.get_html(id_url, method='POST', from_data=id_from_data)
+    info_list = wyy.parse_text(id_text)
+
+    while True:
+        input_index = eval(input("请输入要下载歌曲的序号(-1退出): "))
+        if input_index == -1:
+            break
+        download_info = info_list[input_index]
+        song_d = {
+            "ids": str([download_info[0]]),
+            "level": "standard",
+            "encodeType": "aac",
+            "csrf_token": ""
+        }
+        song_from_data = encrypt.resultEncrypt(str(song_d))
+
+        song_text = wyy.get_html(song_url, method='POST', from_data=song_from_data)
+        wyy.save_file(song_text, download_info)
+
+
